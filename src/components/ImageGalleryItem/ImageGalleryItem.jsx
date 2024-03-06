b9d0eef156f9d9ff8f030d0da284bc2d0752300e
Rafal Lakomiec <rafal.lakomiec@gmail.com>
2024-03-06 20:23:00 +0100

    Refactor ImageGalleryItem comp

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,26 +1,20 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import css from './ImageGalleryItem.module.css';
 
-class ImageGalleryItem extends Component {
-  static propTypes = {
-    keyValue: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired
-  };
-
-  onClickKeyify = () => () => this.props.onClick(this.props.keyValue);
-
-  render() {
-    const { keyValue, image, description } = this.props;
-
-    return (
-      <li key={keyValue} className={css.imageGalleryItem} onClick={this.onClickKeyify()}>
-        <img className={css.imageGalleryItemImage} src={image} alt={description} />
-      </li>
-    );
-  }
+const ImageGalleryItem = ({onClick, keyValue, image, description}) => {
+  const onClickKeyify = () => () => onClick(keyValue);
+  return (
+    <li key={keyValue} className={css.imageGalleryItem} onClick={onClickKeyify()}>
+      <img className={css.imageGalleryItemImage} src={image} alt={description} />
+    </li>
+  );
 }
+
+ImageGalleryItem.propTypes = {
+  keyValue: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+};
 
 export default ImageGalleryItem;
