cf1104fd24bb4b81c29ac5dc70657941cc4eb61d
Rafal Lakomiec <rafal.lakomiec@gmail.com>
2024-03-06 20:27:41 +0100

    Refactor Button comp

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,19 +1,16 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import css from './Button.module.css';
 
-class Button extends Component {
-  static propTypes = {
-    onClick: PropTypes.func.isRequired,
-  };
-
-  render() {
-    return (
-      <button className={css.button} onClick={this.props.onClick}>
-        Load More
-      </button>
-    );
-  }
+const Button = ({onClick}) => {
+  return (
+    <button className={css.button} onClick={onClick}>
+      Load More
+    </button>
+  );
 }
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default Button;
