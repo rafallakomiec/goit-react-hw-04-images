ac789827061ae54fed9ba36b2b03f9b4a22cf198
Rafal Lakomiec <rafal.lakomiec@gmail.com>
2024-03-06 20:19:37 +0100

    Refactor Modal comp

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,35 +1,32 @@
-import { Component } from 'react';
 import css from './Modal.module.css';
 import PropTypes from 'prop-types';
 
-class Modal extends Component {
-  static propTypes = {
-    image: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    handleImageLoaded: PropTypes.func.isRequired,
-    handleModalClose: PropTypes.func.isRequired,
-    isModalOn: PropTypes.bool.isRequired,
-    isSpinnerOn: PropTypes.bool.isRequired
-  };
-
-  render() {
-    return (
-      <div
-        className={this.props.isModalOn ? css.overlayVisible : css.overlay}
-        onClick={this.props.handleModalClose}
-        onKeyUp={this.props.handleModalClose}
-      >
-        <div className={css.modal}>
-          <img
-            className={this.props.isModalOn && !this.props.isSpinnerOn ? css.modalImgVisible : css.modalImg}
-            src={this.props.image}
-            alt={this.props.description}
-            onLoad={this.props.handleImageLoaded}
-          />
-        </div>
+const Modal = ({isModalOn, handleModalClose, isSpinnerOn, image, description, handleImageLoaded}) => {
+  return (
+    <div
+      className={isModalOn ? css.overlayVisible : css.overlay}
+      onClick={handleModalClose}
+      onKeyUp={handleModalClose}
+    >
+      <div className={css.modal}>
+        <img
+          className={isModalOn && !isSpinnerOn ? css.modalImgVisible : css.modalImg}
+          src={image}
+          alt={description}
+          onLoad={handleImageLoaded}
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+Modal.propTypes = {
+  image: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  handleImageLoaded: PropTypes.func.isRequired,
+  handleModalClose: PropTypes.func.isRequired,
+  isModalOn: PropTypes.bool.isRequired,
+  isSpinnerOn: PropTypes.bool.isRequired
+};
 
 export default Modal;
