613da2510c14771ed4123c9cd87fb6d85e0d0198
Rafal Lakomiec <rafal.lakomiec@gmail.com>
2024-03-06 20:25:54 +0100

    Refactor ImageGallery comp

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,29 +1,27 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import css from './ImageGallery.module.css';
 import { nanoid } from 'nanoid';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 
-class ImageGallery extends Component {
-  static propTypes = {
-    images: PropTypes.arrayOf(PropTypes.object).isRequired,
-    openModalHandler: PropTypes.func.isRequired
-  };
-
-  render() {
-    const imageGalleryItems = this.props.images.map(item => {
-      return (
-        <ImageGalleryItem
-          key={nanoid()}
-          keyValue={item.id.toString()}
-          description={item.tags}
-          image={item.webformatURL}
-          onClick={this.props.openModalHandler}
-        />
-      );
-    });
-    return <ul className={css.imageGallery}>{imageGalleryItems}</ul>;
-  }
+const ImageGallery = ({images, openModalHandler}) => {
+  const imageGalleryItems = images.map(item => {
+    return (
+      <ImageGalleryItem
+        key={nanoid()}
+        keyValue={item.id.toString()}
+        description={item.tags}
+        image={item.webformatURL}
+        onClick={openModalHandler}
+      />
+    );
+  });
+  
+  return <ul className={css.imageGallery}>{imageGalleryItems}</ul>;
 }
+
+ImageGallery.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.object).isRequired,
+  openModalHandler: PropTypes.func.isRequired
+};
 
 export default ImageGallery;
